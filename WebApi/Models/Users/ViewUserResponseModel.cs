45b2ff633fc2460daba293cf0a6b5e056dffd0db
Xeretis <70800211+Xeretis@users.noreply.github.com>
2022-12-07 08:09:14 +0100

    Removed email form the view user response

--- WebApi/Models/Users/ViewUserResponseModel.cs
+++ WebApi/Models/Users/ViewUserResponseModel.cs
@@ -3,7 +3,6 @@ namespace WebApi.Models.Users;
 public class ViewUserResponseModel
 {
     public string Id { get; set; }
-    public string Email { get; set; }
     public string Name { get; set; }
     public string UserName { get; set; }
     public string? ProfilePictureUrl { get; set; }
