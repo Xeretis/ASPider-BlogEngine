5be8312683e172f6bdf04d16c2fce0f82292702a
Xeretis <70800211+Xeretis@users.noreply.github.com>
2022-12-07 08:11:25 +0100

    Changed the view user response again, hopefully it's right now

--- WebApi/Models/Users/ViewUserResponseModel.cs
+++ WebApi/Models/Users/ViewUserResponseModel.cs
@@ -12,8 +12,8 @@ public class ViewUserResponseModel
 
 public class ViewUserPostResponseModel
 {
+    public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public string Content { get; set; }
     public string? ThumbnailUrl { get; set; }
 }
